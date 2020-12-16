0488a5cfb325797c8d30490d95d247993e20c118
Gareth Tribello <gareth.tribello@gmail.com>
2020-12-16 17:26:29 +0000

    Added answer

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ def potential(x) :
   energy = 0 
   forces = np.zeros([7,2])
   # Your code to calculate the potential goes here
-  
+  for i in range(1,7) :
+      for j in range(i) :
+          d = x[i,:]-x[j,:]
+          r2 = sum(d*d)
+          r6 = r2*r2*r2
+          r12 = r6*r6
+          energy = energy + 4/r12 - 4/r6
+          pref = 4*( 6/(r6*r2) - 12/(r12*r2) )
+          forces[i,:] =  forces[i,:] - pref*d
+          forces[j,:] =  forces[j,:] + pref*d 
   return energy, forces
   
 # This command reads in the positions that are contained in the file called positions.txt
@@ -25,20 +34,24 @@ for step in range(nsteps) :
   # First update the velocities a half timestep
   # fill in the blanks in the code here
   for i in range(7) : 
-    vel[i][0] = 
-    vel[i][1] = 
+    vel[i][0] = vel[i][0] + 0.5*timestep*forces[i][0]
+    vel[i][1] = vel[i][1] + 0.5*timestep*forces[i][1]
     
   # Now update the positions using the new velocities
   # You need to add code here
-  
+  for i in range(7) :
+    pos[i][0] = pos[i][0] + timestep*vel[i][0]
+    pos[i][1] = pos[i][1] + timestep*vel[i][1]
   
   # Recalculate the forces at the new position
   # You need to add code here
-  eng, forces = 
+  eng, forces = potential(pos)
   
   # And update the velocities another half timestep
   # You need to add code here
-
+  for i in range(7) :
+    vel[i][0] = vel[i][0] + 0.5*timestep*forces[i][0]
+    vel[i][1] = vel[i][1] + 0.5*timestep*forces[i][1]
 
   # I have stored the trajectory here so we can plot how the positions 
   # of each of the atoms changes with time.  
